89b0ff5a2478f056783d12027966826a4d76f86d
user <user@EA07>
2020-09-29 09:50:42 +0800

    1111

--- src/lab1/lab1.java
+++ src/lab1/lab1.java
@@ -4,7 +4,9 @@ public class lab1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("I'm ¤B¤l°a");
+		System.out.println("My nid is D0813276");
+		System.out.println("This is lab 1 hw");
 	}
 
 }
